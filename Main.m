caaccc5a848d2f38b1e363bbe877f8ae4204da14
ShengyiXu <xushengyichn@outlook.com>
2023-07-30 22:44:55 +0200

    update

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Author: xushengyichn 54436848+xushengyichn@users.noreply.github.com Date:
-%LastEditors: xushengyichn xushengyichn@outlook.com
-%LastEditTime: 2023-07-24 01:55:19
+%LastEditors: ShengyiXu xushengyichn@outlook.com
+%LastEditTime: 2023-07-29 22:42:26
 %Description: 计算简支梁施加荷载后的动力响应，并反算出荷载（分别按照集中力和模态力反算）
 %
 %Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
@@ -579,7 +579,7 @@ function node = FindNodewithLocation(loc, node_loc,nodeondeck)
 end
 
 function [S_a, S_v, S_d, n_sensors] = sensor_selection(loc_acc, loc_vel, loc_dis, node_loc, phi,nodeondeck)
-
+    % TODO: check if use matrix index rather than node number
     if ~isempty(loc_acc)
         acc_node = FindNodewithLocation(loc_acc, node_loc,nodeondeck);
         acc_node_list=acc_node(:);
