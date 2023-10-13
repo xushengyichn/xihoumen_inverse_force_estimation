9dde317d4e292dbe375ce3d3224915e0cdc5b7ee
Shengyi <xushengyichn@outlook.com>
2023-10-13 20:46:33 +0800

    add optimization algorithm

--- 20231005 first version/Main.m
+++ 20231005 first version/Main.m
@@ -27,7 +27,8 @@ n = 4;
 [result] = viv2013(n, OFF);
 input.start_time = result.startDate;
 input.end_time = result.endDate;
-input.acc_dir = "F:\test\result";
+% input.acc_dir = "F:\test\result";
+input.acc_dir = "Z:\Drive\Backup\SHENGYI_HP\F\test\result";
 
 input.lambda = 10 ^ (-1);
 input.sigma_p = 10000;
@@ -35,30 +36,44 @@ input.omega_0_variation =1;
 input.Q_value =10 ^ (-8);
 input.R_value = 10 ^ (-6);
 
-[result_Main] = KalmanMain(input,'showtext', false,'showfigure',true);
+[result_Main] = KalmanMain(input,'showtext', false,'showfigure',false);
 logL = result_Main.logL;
 
 
 
 %% optimization logL to get the maximum with changing lambda sigma_p omega_0_variation Q_value R_value
-
+% 在调用 ga 函数之前，您可以这样设置 external_params：
+external_params.figPos = figPos;
+external_params.ON = ON;
+external_params.OFF = OFF;
 % 定义参数的范围
-lb = [-5, 10, 0.8, -10, -10]; % 这里的值是假设的，请根据您的情况进行修改
-ub = [-1, 1e7, 1.2, 0, 0]; % 这里的值也是假设的
+lb = [-5, 10, 0.9, -10, -10]; % 这里的值是假设的，请根据您的情况进行修改
+ub = [-1, 1e5, 1.1, -1, -1]; % 这里的值也是假设的
 
 % 定义整数和连续变量
 IntCon = []; % 如果没有整数变量，否则提供整数变量的索引
 
-options = optimoptions('ga', 'MaxGenerations', 100, 'Display', 'iter');
-[x, fval] = ga(@fitnessFunction, 5, [], [], [], [], lb, ub, [], IntCon, options);
+options = optimoptions('ga', 'MaxGenerations', 100, 'Display', 'iter', 'UseParallel', true);
+[x, fval] = ga(@(params) fitnessFunction(params, external_params), 5, [], [], [], [], lb, ub, [], IntCon, options);
+% options = optimoptions('ga', ...
+%                        'MaxGenerations', 100, ...
+%                        'Display', 'iter', ...
+%                        'OutputFcn', @plotOptimizationOutput); % 设置输出函数
+% [x, fval] = ga(@fitnessFunction, 5, [], [], [], [], lb, ub, [], IntCon, options);
 
+% 保存结果
+save('optimization_results.mat', 'x', 'fval');
 
-function logL = fitnessFunction(params)
+function logL = fitnessFunction(params,external_params)
     input.lambda = 10 ^ params(1);
     input.sigma_p = params(2);
     input.omega_0_variation = params(3);
     input.Q_value = 10 ^ params(4);
     input.R_value = 10 ^ params(5);
+
+     figPos = external_params.figPos;
+    ON = external_params.ON;
+    OFF = external_params.OFF;
 
     input.num_figs_in_row = 12; %每一行显示几个图
     input.figPos = figPos; %图的大小，参数基于InitScript.m中的设置
@@ -71,9 +86,24 @@ function logL = fitnessFunction(params)
     [result] = viv2013(n, OFF);
     input.start_time = result.startDate;
     input.end_time = result.endDate;
-    input.acc_dir = "F:\test\result";
+    % input.acc_dir = "F:\test\result";
+    input.acc_dir = "Z:\Drive\Backup\SHENGYI_HP\F\test\result";
 
     result_Main = KalmanMain(input, 'showtext', false, 'showfigure', false);
     logL = -result_Main.logL; % 因为 ga 试图最小化函数，所以取负数
 end
 
+
+
+function stop = plotOptimizationOutput(optimValues, state)
+    stop = false;
+    switch state
+        case 'init'
+            hold on;
+        case 'iter'
+            % 绘制当前最佳适应度值
+            plot(optimValues.iteration, optimValues.bestfval, '.b');
+        case 'done'
+            hold off;
+    end
+end
