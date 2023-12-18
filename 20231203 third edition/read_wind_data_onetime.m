d4130d0fb562ced87f3d94a73e8bdcdbe84d76ea
ShengyiXu <xushengyichn@outlook.com>
2023-12-18 12:06:38 +0100

    update read wind date file

--- 20231203 third edition/read_wind_data_onetime.m
+++ 20231203 third edition/read_wind_data_onetime.m
@@ -55,6 +55,13 @@ function [result] = read_wind_data_onetime(timestamps,duration,dirName)
         end
     end
     result_table=[];
+    UA1 =[];
+    UA2 =[];
+    UA3 =[];
+    UA4 =[];
+    UA5 =[];
+    UA6 =[];
+
     for k1  = 1:length(timestamps)
         timestamp = timestamps(k1);
         start_time = timestamp - duration / 2;
@@ -111,21 +118,49 @@ function [result] = read_wind_data_onetime(timestamps,duration,dirName)
         result_6=cal_wind_property(U_x_6,U_y_6,U_z_6,45);
         resultsTable_UA6 = struct2table(result_6); % Convert struct result to table
 
-        result_table_temp=[resultsTable_UA1;resultsTable_UA2;resultsTable_UA3;resultsTable_UA4;resultsTable_UA5;resultsTable_UA6;];
-        
-        % 假设 strArray 是一个单元数组，长度与 T 的行数相同
-        strArray = {'UA1'; 'UA2'; 'UA3'; 'UA4'; 'UA5';'UA6'}; % 根据需要填充
-        
-        % 添加到表格
-        result_table_temp.property = strArray;
-        result_table_temp.timestamp =  repmat(timestamp, height(result_table_temp), 1);
-        % 将 NewColumn 移动到第一列
-        result_table_temp = movevars(result_table_temp, 'property', 'Before', result_table_temp.Properties.VariableNames{1});
-        result_table_temp = movevars(result_table_temp, 'timestamp', 'Before', result_table_temp.Properties.VariableNames{1});
+        resultsTable_UA1.timestamp = timestamp;
+        resultsTable_UA2.timestamp = timestamp;
+        resultsTable_UA3.timestamp = timestamp;
+        resultsTable_UA4.timestamp = timestamp;
+        resultsTable_UA5.timestamp = timestamp;
+        resultsTable_UA6.timestamp = timestamp;
 
-        result_table=[result_table;result_table_temp];
+        resultsTable_UA1 = movevars(resultsTable_UA1, 'timestamp', 'Before', resultsTable_UA1.Properties.VariableNames{1});
+        resultsTable_UA2 = movevars(resultsTable_UA2, 'timestamp', 'Before', resultsTable_UA2.Properties.VariableNames{1});
+        resultsTable_UA3 = movevars(resultsTable_UA3, 'timestamp', 'Before', resultsTable_UA3.Properties.VariableNames{1});
+        resultsTable_UA4 = movevars(resultsTable_UA4, 'timestamp', 'Before', resultsTable_UA4.Properties.VariableNames{1});
+        resultsTable_UA5 = movevars(resultsTable_UA5, 'timestamp', 'Before', resultsTable_UA5.Properties.VariableNames{1});
+        resultsTable_UA6 = movevars(resultsTable_UA6, 'timestamp', 'Before', resultsTable_UA6.Properties.VariableNames{1});
+
+        UA1 = [UA1;resultsTable_UA1];
+        UA2 = [UA2;resultsTable_UA2];
+        UA3 = [UA3;resultsTable_UA3];
+        UA4 = [UA4;resultsTable_UA4];
+        UA5 = [UA5;resultsTable_UA5];
+        UA6 = [UA6;resultsTable_UA6];
+
+
+        
+        % result_table_temp=[resultsTable_UA1;resultsTable_UA2;resultsTable_UA3;resultsTable_UA4;resultsTable_UA5;resultsTable_UA6;];
+        
+        % % 假设 strArray 是一个单元数组，长度与 T 的行数相同
+        % strArray = {'UA1'; 'UA2'; 'UA3'; 'UA4'; 'UA5';'UA6'}; % 根据需要填充
+        
+        % % 添加到表格
+        % result_table_temp.property = strArray;
+        % result_table_temp.timestamp =  repmat(timestamp, height(result_table_temp), 1);
+        % % 将 NewColumn 移动到第一列
+        % result_table_temp = movevars(result_table_temp, 'property', 'Before', result_table_temp.Properties.VariableNames{1});
+        % result_table_temp = movevars(result_table_temp, 'timestamp', 'Before', result_table_temp.Properties.VariableNames{1});
+
+        % result_table=[result_table;result_table_temp];
     end
         
-    result =result_table;
+    result.UA1 = UA1;
+    result.UA2 = UA2;
+    result.UA3 = UA3;
+    result.UA4 = UA4;
+    result.UA5 = UA5;
+    result.UA6 = UA6;
 
 end
