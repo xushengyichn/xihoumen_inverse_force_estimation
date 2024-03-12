bf82e820889229e6ca4c5ff3887e30f520f9117d
ShengyiXu <xushengyichn@outlook.com>
2024-03-12 15:30:11 +0100

    update for time delay calculation

--- 20231203 third edition/sync_cov.m
+++ 20231203 third edition/sync_cov.m
@@ -1,4 +1,4 @@
-function [delay2_seconds, delay3_seconds] = sync_cov(signal1, signal2, signal3, sampleRate,maxDelaySeconds)
+function [delay2_seconds, delay3_seconds,results] = sync_cov(signal1, signal2, signal3, sampleRate,maxDelaySeconds)
     % 定义最大延时时间（秒）
 
     % 将最大延时时间转换为样本数
@@ -19,8 +19,12 @@ function [delay2_seconds, delay3_seconds] = sync_cov(signal1, signal2, signal3, 
     delay3_samples = lags3(I3);
 
     % 将延时从样本数转换为秒
-    delay2_seconds = delay2_samples / sampleRate;
-    delay3_seconds = delay3_samples / sampleRate;
+    delay2_seconds = int32(delay2_samples / sampleRate);
+    delay3_seconds = int32(delay3_samples / sampleRate);
+    results.corr2=corr2;
+    results.corr3=corr3;
+    results.lags2=lags2;
+    results.lags3=lags3;
 
 % 打印延时结果（以秒为单位）
     fprintf('Signal 2 delay relative to Signal 1: %f seconds\n', delay2_seconds);
